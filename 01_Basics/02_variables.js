12ef9e6f3b9ab3509f5c373b55c2d0df3ef0d4c5
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-10-24 11:42:22 +0000

    Updated 02_variables.js and one.js

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,8 @@
+const studentID = 1234
+var studentEmail = 'hello@1234'
+let studentPassword = 'deiofes'
+
+
+// studentPassword= 'dfgesf'   not a problem
+// studentID = 3211  it is wrong
+studentAddress = 'delhi'
