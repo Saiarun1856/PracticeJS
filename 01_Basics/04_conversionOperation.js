c578729be61eb29cfde1d58832e26f3c0f0c0045
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-10-25 13:57:00 +0000

    ConversionOperations

---
+++ 01_Basics/04_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+let value = Number(score)
+console.log(value);
+
+
+
+// Number conversion
+// "33" => 33
+//"33abf" =>NaN(Not a Number)
+//true=> 1
+// undefined => NaN
+
+// boolean conversion
+
+// 1 => true
+//0 => false
+// "" => false
+// "Lsjfa" => true
+
+
+
+
+
+
+ 
