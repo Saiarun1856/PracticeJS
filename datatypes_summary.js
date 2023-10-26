b7eaaf4c81b2fa35edfb8bf936ed6e1bbd1b3f1d
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-10-26 10:26:00 +0000

    summary of data types

---
+++ datatypes_summary.js
@@ -0,0 +1,9 @@
+// Primitive
+
+// 7 types : string, Number, Boolean, Bigint, symbol, undefiend, Null
+
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+// Non-Primitive(Reference)>>>>
+
+// Arrays,Objects,Functions
