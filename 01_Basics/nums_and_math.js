438633f0bfd550d7cba489e2a7a1cd258b2cbd3a
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-11-01 14:15:35 +0000

    Math and Num

---
+++ 01_Basics/nums_and_math.js
@@ -0,0 +1,29 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+
+// const otherNumber = 123.8923
+// console.log(otherNumber.toPrecision(4));
+
+// const usNumbers = 1000000000000
+// console.log(usNumbers.toLocaleString('en-In'))
+
+
+
+// ++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.round(4.3));
+
+
+// console.log(Math.round(Math.random()*100) +1)
+
+const min = 20
+const max = 30
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
