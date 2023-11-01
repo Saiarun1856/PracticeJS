ebe1d268c13059231332833e205f44ee0cdb0586
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-11-01 14:32:08 +0000

    Date

---
+++ 01_Basics/date.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+let newDate = new Date()
+console.log(newDate.getDate())
+console.log(newDate.getMonth()+1)
+console.log(newDate.getFullYear())
+
