dca225fbf44d5cfacfc1248a9d0387ae6567e97d
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-11-03 11:56:36 +0000

    Arrays

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,29 @@
+let myarr = [1,2,3,4,5,6]
+
+
+// Array methods
+
+// myarr.push(5)
+// console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
+
+// console.log(myarr.includes(7))
+// console.log(myarr.indexOf(2))
+
+// let newarr = myarr.join()
+// console.log(typeof newarr )
+// console.log(newarr);
+
+
+// let newarr = myarr.slice(1,3)
+// console.log("A",myarr)
+// console.log("B",newarr)
+
+// const newsarr = myarr.splice(1,3)
+// console.log("D",myarr)
+// console.log("C", newsarr)
+
+
+
+
