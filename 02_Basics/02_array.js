7e953b8eae4d181adf863ca065d562962674d8fb
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-11-03 13:08:07 +0000

    arrays

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,29 @@
+const marver_heros = ["thor","ironman", "spider-man"]
+const dc_heros = ["Batman","flash","superman"]
+
+// marver_heros.push(dc_heros)
+// console.log(marver_heros)
+
+
+// let all_heros = marver_heros.concat(dc_heros)
+// console.log(marver_heros)
+// console.log(all_heros)
+
+
+// const all_new_heros = [...marver_heros,...dc_heros]
+// console.log(all_new_heros);
+
+
+// const all_full_array = [123,"dafdwaf",2,3,4,5,6,[2,3,4,[34,53]]]
+// console.log(all_full_array.flat(Infinity))
+
+
+console.log(Array.isArray(marver_heros))
+console.log(Array.from("Arun"))
+
+let new1 = 200
+let new2 = 300
+let new3 = 400
+
+console.log(Array.of(new1,new2,new3))
+
