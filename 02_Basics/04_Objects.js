4da437366d06202376848930aa72fa54827de1f5
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-11-09 14:08:04 +0000

    Objects

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -23,7 +23,20 @@ const obj2 = {c:1,d:2}
 const obj3 = Object.assign({},obj1,obj2)
 // const obj3 = {...obj1,...obj2}
 
-console.log(obj3)
+// console.log(obj3)
 
-console.log(Object.keys(regularUser))  // returns a array with keys
-console.log(Object.values(regularUser))  // returns a array with Values
+// console.log(Object.keys(regularUser))  // returns a array with keys
+// console.log(Object.values(regularUser))  // returns a array with Values
+
+
+// object destructuring
+const myObj = {
+    names:"Sai Aruns",
+    id:20
+}
+
+const {names : yes} = myObj
+
+console.log(yes)
+
+
