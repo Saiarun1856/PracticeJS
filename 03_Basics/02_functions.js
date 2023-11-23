6cebdd8a213718e5a2f52cebdfe1bb38cded6a74
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-11-23 07:56:08 +0000

    functions

--- 03_Basics/02_functions.js
+++ 03_Basics/02_functions.js
@@ -1,4 +1,23 @@
-function calculatecartPrice(...num1){
+function calculatecartPrice(val1,val2,...num1){
     return num1
 }
 console.log(calculatecartPrice(5,10,30,20,30))
+
+
+const user = {
+    username : "Arun",
+    price:11
+}
+
+function values(anyObject){
+    console.log(`user is ${anyObject.username} and price is ${anyObject.price}`)
+}
+values(user)
+
+const newArray = [12,34,5,6,7,8,2]
+
+function getArray(values){
+    return values[1]
+}
+result = getArray(newArray)
+console.log(result);
